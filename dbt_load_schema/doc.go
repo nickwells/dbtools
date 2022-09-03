06d81ced8007f9c0269831ba0a4e723e8bd05c38
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 23:17:45 +0100

    refactor: minor changes to keep gofumpt quiet

--- dbt_load_schema/doc.go
+++ dbt_load_schema/doc.go
@@ -1,7 +1,5 @@
 /*
-
 dbt_load_schema is a command which applies database schema definition files
 to the database server.
-
 */
 package main
--- internal/dbtcommon/doc.go
+++ internal/dbtcommon/doc.go
@@ -1,6 +1,4 @@
 /*
-
 Package dbtcommon implements some functions used by the dbtools commands.
-
 */
 package dbtcommon
